6aa5cbc8721a9301ce3d157c10085b246c05e642
Yannick Spreen <yannick.spreen@gmail.com>
2019-11-26 15:20:03 -0500

    Change last update format.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -24,7 +24,7 @@ module.exports = {
         transformer: (timestamp, lang) => {
           const moment = require("moment");
           moment.locale(lang);
-          return moment(timestamp).fromNow();
+          return moment(timestamp).format("YYYY-MM-DD HH:mm");
         }
       }
     ]
